902ccd66844e46046fd87e7905294891eb33ae88
foryou929 <zxcvwert3457@gmail.com>
2024-04-07 11:48:04 +0900

    feat: update main

--- Console/Console.cpp
+++ Console/Console.cpp
@@ -3,19 +3,49 @@
 using namespace std;
 
 bool getLine(FILE* fp, char* buf) {
-	return fgets(buf, 2000, fp) != NULL;
+	return fgets(buf, 65500, fp) != NULL;
 }
+
+FILE * openFile() {
+	static int nFiles = 0;
+	nFiles++;
+	char outfilename[260];
+	sprintf(outfilename, "out%d.json", nFiles);
+	return fopen(outfilename, "w");
+}
+
+char buf[65536];
+char result[65536];
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
 		cout << "Please input a file.\n";
 		return 0;
 	}
+
 	FILE* fp = fopen(argv[1], "r");
-	char buf[2048];
+	FILE* outfp = openFile();
 	while (getLine(fp, buf)) {
-		int ii = 0;
+		char * s1 = strstr(buf, "[ [ [");
+		char * s2 = strstr(buf, "] ] ]");
+		int nLen = strlen(buf);
+		if (s1 && s2) {
+			int idx1 = s1 - buf;
+			int idx2 = s2 - buf;
+			static int nLines = 0;
+			if (nLines == 1000000) {
+				nLines = 0;
+				fflush(outfp);
+				fclose(outfp);
+				outfp = openFile();
+			}
+			strcpy(result, buf + idx1);
+			result[idx2 - idx1 + 5] = 0;
+			fprintf(outfp, "%s\n", result);
+			nLines++;
+		}
 	}
 	fclose(fp);
+	fclose(outfp);
 	return 0;
 }
